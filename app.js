7ce78ed324bd4c5db58991121d5da8ea6ade4a69
wanziee <ichwanalghifa@gmail.com>
2025-06-30 11:16:37 +0700

    alloweorigins

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ const allowedOrigins = [
   'http://localhost:3001',
   'http://localhost:5173',
   'http://127.0.0.1:3001',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
+  'https://radiant-nourishment.up.railway.app'
 ];
 
 // Apply CORS middleware
